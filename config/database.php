3af0ffd7bbc0a8cc58eca9568ee7a80613c6eddd
FredoRedfield <fio_iracheta@hotmail.com>
2018-08-24 16:12:11 -0500

    conectadocreo

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'vhw3t8e71xdz9k14.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'lme2ccvlmkhy0rhb'),
+            'username' => env('DB_USERNAME', 'sr1pczc9bfd5j942'),
+            'password' => env('DB_PASSWORD', 'jizuqd3kmmg7f8w7'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
