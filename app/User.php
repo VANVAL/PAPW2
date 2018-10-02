a49ffcf3b816ba1fbc9cc46d34d5d9fa67862546
Vanessa Valdivia <yumivan@hotmail.com>
2018-10-02 00:31:19 -0500

    Implementado mails

--- app/User.php
+++ app/User.php
@@ -4,10 +4,12 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\MustVerifyEmail;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
+    
 
     /**
      * The attributes that are mass assignable.
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 
-Auth::routes();
+Auth::routes(['Verify'=>TRUE]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
